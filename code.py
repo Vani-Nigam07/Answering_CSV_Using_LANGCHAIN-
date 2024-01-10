d9027c0e713388d3e3029425e47f70622ad134b7
Vani <vani07nigam@gmail.com>
2024-01-10 19:08:47 +0530

    updated

--- code.py
+++ code.py
@@ -21,7 +21,7 @@ def main():
 
     st.set_page_config(page_title="Knowwize Coding Assessment", page_icon="🦜")
     print(st.secrets)
-    os.environ["OPENAI_API_KEY"] = st.secrets["KEY"]
+    os.environ["OPENAI_API_KEY"] = st.secrets["general"]["KEY"]
     st.header("Knowwize Coding Assessment")
     st.write("This CSV includes Placement related statistics of an institute👋")
 
